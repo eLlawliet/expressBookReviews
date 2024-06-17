80ce406588c452cf914f327a5ae93103d5047ff4
iel <herobrinealegend@gmail.com>
2024-06-17 18:44:31 +0700

    Task10 - Task13

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,42 +25,47 @@ public_users.post("/register", (req,res) => {
 });
   
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  let readable_book = JSON.stringify({books}, null, 2)
-  return res.status(200).send(readable_book);
+public_users.get('/', async function (req, res) {
+  try {
+    const response = await axios.get('http://localhost:5000/booksdb');
+    let readable_book = JSON.stringify(response.data, null, 2);
+    return res.status(200).send(readable_book);
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred.", error });
+  }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn
-  return res.status(200).json(books[isbn]);
- });
+public_users.get('/isbn/:isbn', async function (req, res) {
+  try {
+    const isbn = req.params.isbn;
+    const response = await axios.get(`http://localhost:5000/books/${isbn}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred.", error });
+  }
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const authorName = req.params.author;
-  let booksByAuthor = {};
-
-  for (let id in books) {
-    if (books[id].author === authorName) {
-      booksByAuthor[id] = books[id];
-    }
+public_users.get('/author/:author', async function (req, res) {
+  try {
+    const authorName = req.params.author;
+    const response = await axios.get(`http://localhost:5000/books/author/${authorName}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred.", error });
   }
-  return res.status(200).json(booksByAuthor);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  const titleBooks = req.params.title;
-  let booksByTitle = {}
-  
-  for (let id in books) {
-    if (books[id].title === titleBooks){
-      booksByTitle[id] = books[id]
-    }
+public_users.get('/title/:title', async function (req, res) {
+  try {
+    const titleBooks = req.params.title;
+    const response = await axios.get(`http://localhost:5000/books/title/${titleBooks}`);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    return res.status(500).json({ message: "An error occurred.", error });
   }
-  return res.status(200).json(booksByTitle)
 });
 
 //  Get book review
